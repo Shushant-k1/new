00db2afd9197f15a871696a3d256191ce776144a
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-11-24 14:59:44 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 st.title('Potato Leaf Disease Prediction')
 
 def main() :
-    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    file_uploaded = st.file_uploader('Choose an image...')
     if file_uploaded is not None :
         image = Image.open(file_uploaded)
         st.write("Uploaded Image.")
@@ -34,7 +34,7 @@ def predict_class(image) :
         classifier_model = keras.models.load_model(r'final_model.h5', compile = False)
 
     shape = ((256,256,3))
-    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])     # ye bhi kaam kar raha he
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])
     test_image = image.resize((256, 256))
     test_image = keras.preprocessing.image.img_to_array(test_image)
     test_image /= 255.0
