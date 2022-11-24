94741366a1259131a6b44c41b75796c8da57d7d7
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-11-24 14:52:50 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 st.title('Potato Leaf Disease Prediction')
 
 def main() :
-    file_uploaded = st.file_uploader('Choose an image...')
+    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
     if file_uploaded is not None :
         image = Image.open(file_uploaded)
         st.write("Uploaded Image.")
@@ -27,22 +27,22 @@ def main() :
         st.pyplot(figure)
         result, confidence = predict_class(image)
         st.write('Prediction : {}'.format(result))
-        st.write('Confidence : {}%'.format(confidence))
+        #st.write('Confidence : {}%'.format(confidence))
 
 def predict_class(image) :
     with st.spinner('Loading Model...'):
         classifier_model = keras.models.load_model(r'final_model.h5', compile = False)
 
-    shape = ((224,224,3))
+    shape = ((256,256,3))
     model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])     # ye bhi kaam kar raha he
-    test_image = image.resize((224, 224))
+    test_image = image.resize((256, 256))
     test_image = keras.preprocessing.image.img_to_array(test_image)
     test_image /= 255.0
     test_image = np.expand_dims(test_image, axis = 0)
     class_name = ['Potato__Early_blight', 'Potato__Late_blight', 'Potato__healthy']
 
-    prediction = model.predict(test_image)[0]
-    confidence = round(100 * (np.max(prediction)[0]), 2)
+    prediction = model.predict(test_image)
+    confidence = round(100 * (np.max(prediction[0])), 2)
     final_pred = class_name[np.argmax(prediction)]
     return final_pred, confidence
 
@@ -71,7 +71,6 @@ a:hover,  a:active {
 <p align="center"> Developed with ❤ by Deepak kumar , Shushant Kumar and Shreyansh </p>
 </div>
         """
-
 st.markdown(footer, unsafe_allow_html = True)
 
 if __name__ == '__main__' :
